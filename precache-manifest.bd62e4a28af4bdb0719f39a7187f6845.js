b7615da42828536d4a7491e3f602c90c38933246
Kayode Alao <Oluwakayode.alao@gmail.com>
2019-11-29 21:22:15 +0100

    Add files via upload

---
+++ precache-manifest.bd62e4a28af4bdb0719f39a7187f6845.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f62addc531bb609a5db3d42a735050",
+    "url": "/greatkay-olowo/ReactJS-Quote-Generator/index.html"
+  },
+  {
+    "revision": "6f14513925331017cd2a",
+    "url": "/greatkay-olowo/ReactJS-Quote-Generator/static/css/main.bc6d750a.chunk.css"
+  },
+  {
+    "revision": "21d69d1a1e712359d0b8",
+    "url": "/greatkay-olowo/ReactJS-Quote-Generator/static/js/2.f27369f9.chunk.js"
+  },
+  {
+    "revision": "6f14513925331017cd2a",
+    "url": "/greatkay-olowo/ReactJS-Quote-Generator/static/js/main.db819492.chunk.js"
+  },
+  {
+    "revision": "b7c5dbffbcef42577e26",
+    "url": "/greatkay-olowo/ReactJS-Quote-Generator/static/js/runtime-main.e45b618d.js"
+  }
+]);
